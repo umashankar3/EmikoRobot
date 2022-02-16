84521212114b6f6ca85cc03353725cec5d9414d3
Umashankar <91821821+umashankar3@users.noreply.github.com>
2022-02-16 22:45:24 +0530

    Update config.py

--- EmikoRobot/config.py
+++ EmikoRobot/config.py
@@ -17,14 +17,14 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1606221784  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "excrybaby"
+    OWNER_ID = 1788262293  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "umashankar31"
     SUPPORT_CHAT = "emikosupport"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001432609692
+        -1001752797656
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001150905176
+        -1001752797656
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
@@ -35,7 +35,7 @@ class Config(object):
     INFOPIC = True
     URL = None
     SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
-    SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
+    SPAMWATCH_SUPPORT_CHAT = "@shayari_jok"
 
     # OPTIONAL
     ##List of id's -  (not usernames) for users which have sudo access to the bot.
